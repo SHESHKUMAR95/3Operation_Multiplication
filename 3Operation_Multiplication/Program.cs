a826ad5b84f78aca137adb1aa606a1dd4ee0d92b
SHESHKUMAR95 <148970064+SHESHKUMAR95@users.noreply.github.com>
2023-11-30 12:57:14 +0530

    Initial commit

---
+++ 3Operation_Multiplication/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3Operation_Multiplication
+{
+    internal class Program
+    {
+        public void Multiplication()
+        {
+            int a = 10, b = 20;
+            int c = a * b;
+            Console.WriteLine("Multipliation : " + c);
+        }
+        static void Main(string[] args)
+        {
+            Program op = new Program();
+            op.Multiplication();
+            Console.ReadLine(); 
+        }
+    }
+}
